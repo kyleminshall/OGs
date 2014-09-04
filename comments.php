60c84171bbd0ca899bdb80a88e912046e92a600b
Kyle Minshall <kyleminshall@gmail.com>
2014-09-04 16:48:44 -0700

    Visual refinements

--- comments.php
+++ comments.php
@@ -178,7 +178,7 @@
 			$reply = stripslashes(submit::auto_link_text($replies2->reply));
   			echo '<tr style="background-color:#f6f6f6;">'; 
   			echo '<td colspan="4" style="display:inline-block"> 
-					<div">
+					<div>
 						<img src="'.$profile.'" alt="Profile" height="40px" width="40px"/>
 					</div>
 				    <div>
