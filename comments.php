2037a6e0ccefe0630f35a266eb5114f6db11d5b3
Kyle Minshall <kyleminshall@gmail.com>
2014-09-04 08:17:42 -0700

    Modifying likes text

--- comments.php
+++ comments.php
@@ -117,7 +117,7 @@
 			echo '<p style="font-size:12px;padding:0;text-align:left">';
 			while($people2 = mysql_fetch_object($people))
 			{
-				echo ''.$people2->username;
+				echo '<b>'.$people2->username.'</b>';
 				if($count > 1) echo ', ';
 				$count--;
 			}
