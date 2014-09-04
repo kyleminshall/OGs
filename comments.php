73612414a2765cbfeec82317c138f3fe1035a043
Kyle Minshall <kyleminshall@gmail.com>
2014-09-04 16:46:11 -0700

    Visual refinements

--- comments.php
+++ comments.php
@@ -178,10 +178,10 @@
 			$reply = stripslashes(submit::auto_link_text($replies2->reply));
   			echo '<tr style="background-color:#f6f6f6;">'; 
   			echo '<td colspan="4"> 
-					<div valign="top">
+					<div style="float:left;width:40px"valign="top">
 						<img src="'.$profile.'" alt="Profile" height="40px" width="40px"/>
 					</div>
-				    <div>
+				    <div style="float:left">
 						<p style="font-size:14px;color:000;margin:0;padding-left:0">
 							<b>'.stripslashes($replies2->username).'</b> '.stripslashes($reply).'<br>
 							<span style="font-size:12px;color:#494949;">'.$replied.'</span>
