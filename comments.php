4a086ca59639d1b210eb26c84cb7361143a59bf3
Kyle Minshall <kyleminshall@gmail.com>
2014-09-04 15:59:52 -0700

    Profile pictures?

--- comments.php
+++ comments.php
@@ -104,7 +104,7 @@
 		$people = mysql_query("SELECT username FROM likes WHERE post='$post_number'");
 		$user_liked = mysql_num_rows(mysql_query("SELECT username FROM likes WHERE username='$username' AND post='$post_number'")) > 0;
 		$profile = mysql_result(mysql_query("SELECT profile FROM OGs WHERE username='$info2->username'"),0);
-		echo '<td style="width:20px"><img src="'.$profile.'" alt="Profile" height="50px" width="50px"/></td>';
+		echo '<td style="width:45px"><img src="'.$profile.'" alt="Profile" height="50px" width="50px"/></td>';
 		echo '<td style="width:65%">
 			<p style="font-size:18px;color:000">
 			<b>'.stripslashes($info2->username).'</b>
