0e038ed4fa54e2316b3d66759dcb601faabef533
Kyle Minshall <kyleminshall@gmail.com>
2014-09-04 16:31:05 -0700

    User photos to comments?

--- comments.php
+++ comments.php
@@ -104,9 +104,9 @@
 		$people = mysql_query("SELECT username FROM likes WHERE post='$post_number'");
 		$user_liked = mysql_num_rows(mysql_query("SELECT username FROM likes WHERE username='$username' AND post='$post_number'")) > 0;
 		$profile = mysql_result(mysql_query("SELECT profile FROM OGs WHERE username='$info2->username'"),0);
-		echo '<td style="width:40px"><img src="'.$profile.'" alt="Profile" height="50px" width="50px"/></td>';
+		echo '<td style="width:40px"><img src="'.$profile.'" alt="Profile" height="40px" width="40px"/></td>';
 		echo '<td style="width:65%">
-			<p style="font-size:18px;color:000">
+			<p style="font-size:18px;color:000;padding-left:0">
 			<b>'.stripslashes($info2->username).'</b>
 			<br>
 			<span style="font-size:12px;color:#494949;">'.$submitted.'</span>
