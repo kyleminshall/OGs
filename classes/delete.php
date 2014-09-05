eb659067c377736612270a2e8e7bf6c436262192
Kyle Minshall <kyleminshall@gmail.com>
2014-09-05 12:41:36 -0700

    Debugging

--- classes/delete.php
+++ classes/delete.php
@@ -3,8 +3,7 @@
 	if(!isset($_SESSION)) //Pretty much included in every single class. Have to make sure the session is set
 	{ 
 		session_start(); 
-	} 
-	echo 'Error here?';
+	}
 	
 	$con=mysql_connect("localhost","KyleM","Minshall1!"); //Connect to the database
 	$db_selected = mysql_select_db("Site", $con);
