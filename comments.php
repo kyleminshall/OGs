db47701250a523deb5c656d281f75c354a7802b3
Kyle Minshall <kyleminshall@gmail.com>
2014-09-05 12:53:59 -0700

    Updating progress and fixing like button

--- comments.php
+++ comments.php
@@ -196,7 +196,7 @@
 		echo '<tr style="background-color:#dddddd;">'; 
 		echo '<td colspan="4" style="padding-left: 10px;">
 				<div align="left" style="display:inline">
-				<p style="font-size:12px;padding:0;text-align:left;display:inline">
+				<p style="font-size:12px;padding:0;text-align:left;display:inline;width:34px">
 					<a id="like_'.$post_number.'" style="text-decoration:none;color:#1F80C9;" href="#" onclick="like_add('.$post_number.');return false;">'.$button.'</a> 
 				</p>
 				</div>';  //HTML for the Like button (Not actually a button. It's a link. Hence the href=#. Executes like_add on click)
