6716e734de1651095a8c72ed855db8acbbe13091
Kyle Minshall <kyleminshall@gmail.com>
2014-09-05 12:43:49 -0700

    Debugging

--- classes/delete.php
+++ classes/delete.php
@@ -1,23 +1,12 @@
 <?php
 
-	if(!isset($_SESSION)) //Pretty much included in every single class. Have to make sure the session is set
-	{ 
-		session_start(); 
-	}
-	
-	$con=mysql_connect("localhost","KyleM","Minshall1!"); //Connect to the database
-	$db_selected = mysql_select_db("Site", $con);
+	include 'like.php';
 	
 	if(isset($_POST['post_id'], $_SESSION['username']) && exists($_POST['post_id']))
 	{
 		$post_id = (int)$_POST['post_id'];
 
 		mysql_query("UPDATE posts SET deleted=1 WHERE id='$post_id'");
-		return "Success";
 	}	
-	else 
-	{
-		return "Error somewhere";
-	}
 	
 ?>
