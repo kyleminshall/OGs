b2b0ebae50f30dfe353bf7899bf1627c9365d202
Kyle Minshall <kyleminshall@gmail.com>
2014-09-05 12:03:42 -0700

    Adding delete button for posts

--- comments.php
+++ comments.php
@@ -195,14 +195,18 @@
 		
 		echo '<tr style="background-color:#dddddd;">'; 
 		echo '<td colspan="4" style="padding-left: 10px;">
-				<p style="font-size:12px;padding:0;text-align:left">
+				<div align="left">
+				<p style="font-size:12px;padding:0;text-align:left;display:inline">
 					<a id="like_'.$post_number.'" style="text-decoration:none;color:#1F80C9;" href="#" onclick="like_add('.$post_number.');return false;">'.$button.'</a> 
-				</p>';  //HTML for the Like button (Not actually a button. It's a link. Hence the href=#. Executes like_add on click)
+				</p>
+				</div>';  //HTML for the Like button (Not actually a button. It's a link. Hence the href=#. Executes like_add on click)
 		if($info2->username === $username)
 		{
-			echo '<p style="font-size:12px;padding:0;text-align:right">
+			echo '<div align="right">
+					<p style="font-size:12px;padding:0;text-align:right;display:inline;">
 						<a id="delete_'.$post_number.'" style="text-decoration:none;color:#1F80C9;" href="#" onclick="delete('.$post_number.');return false;">Delete</a> 
-				  </p>';
+				  </p>
+				  </div>';
 		}
 		echo '</td>';
 		echo '</tr>';
