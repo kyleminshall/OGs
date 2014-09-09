5d750a24831cce1ff52ca3a8c33704ba5681e947
Kyle Minshall <kyleminshall@gmail.com>
2014-09-09 01:42:56 -0700

    Fixing JS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 function remove_activity() {
-	$.post('classes/update_login.php', {post_id:post_id}, function(data) {
-		like_get(post_id);
-	}).done(
-		$(".activity").fadeOut(800, function() {
-		$(".activity").remove();
-	}));
+	$.post('classes/update_login.php');
+	
+	$(".activity").fadeOut(800, function() {
+			$(".activity").remove();
+	});
 }
