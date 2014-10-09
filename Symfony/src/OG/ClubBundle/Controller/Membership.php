561a92d9d9a044e19a295a304ebb90e98f7250e4
Kyle Minshall <kyleminshall@gmail.com>
2014-10-08 22:30:41 -0700

    Fixed?

--- Symfony/src/OG/ClubBundle/Controller/Membership.php
+++ Symfony/src/OG/ClubBundle/Controller/Membership.php
@@ -74,12 +74,7 @@ class Membership
 	
 	*/
 	static function validateUser($username, $password)
-	{
-		if(!isset($session)) //Checks if the session is set
-		{ 
-			$session = new Session(); //Starts a PHP session (cookie)
-		} 
-        
+	{   
 		if(is_null($username))
 		{
 			return "Please enter your username"; //Let them know they forgot to enter a username
