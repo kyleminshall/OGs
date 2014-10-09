004449d9eb1b7cbbbf2cb044f8d26bf97182ba4d
Kyle Minshall <kyleminshall@gmail.com>
2014-10-08 22:16:56 -0700

    Fixed?

--- Symfony/src/OG/ClubBundle/Controller/PageController.php
+++ Symfony/src/OG/ClubBundle/Controller/PageController.php
@@ -228,7 +228,7 @@ class PageController extends Controller
         
         $session = $this->getRequest()->getSession();
         
-        if(is_null($response) && Membership::confirm($session))
+        if(!isset($response) && Membership::confirm($session))
             return $this->redirect($this->generateUrl('index'));
         else if(isset($response))
             return $this->render('OGClubBundle:Page:login.html.twig', array('error' => $response));
