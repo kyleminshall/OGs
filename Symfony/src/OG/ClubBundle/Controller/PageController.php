d5e87e784f991a6bbb6e32aad4025739980745d9
Kyle Minshall <kyleminshall@gmail.com>
2015-01-03 06:00:54 -0800

    Yo

--- Symfony/src/OG/ClubBundle/Controller/PageController.php
+++ Symfony/src/OG/ClubBundle/Controller/PageController.php
@@ -79,6 +79,8 @@ class PageController extends Controller
         
         if($request->getMethod() == "POST" && $request->get('yo', false))
         {
+            echo("Here");
+            die;
             $name = $_POST['yoname'];
             $query = "UPDATE OGs SET Yo='$name' WHERE username='$username'";
         }
